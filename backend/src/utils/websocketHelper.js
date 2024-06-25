e78464777d60ba7b9a02e7950dd2151c5d8a4cab
ttmiirco94 <66681286+ttmiirco94@users.noreply.github.com>
2024-06-25 10:53:24 +0200

    Update websocketHelper.js
    remvoe comments

--- backend/src/utils/websocketHelper.js
+++ backend/src/utils/websocketHelper.js
@@ -7,7 +7,6 @@ exports.initializeWebSocket = async (wss) => {
     logger.info('Initialize WebSocket');
     await wss.on('connection', async (ws) => {
         await getTestsAndOutputs().then(results => {
-            //logger.info(JSON.stringify(results, null, 2));
             ws.send(JSON.stringify(results));
         }).catch(error => {
             logger.error('Broadcasting tests failed: ', error);
@@ -18,7 +17,6 @@ exports.initializeWebSocket = async (wss) => {
 exports.broadcastTests = async (wss) => {
     logger.info('Broadcasting tests to all WebSocket clients');
     await getTestsAndOutputs().then(results => {
-        //logger.info(JSON.stringify(results, null, 2));
         wss.clients.forEach((client) => {
             if (client.readyState === 1) {
                 client.send(JSON.stringify(results));
