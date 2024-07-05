e46b12366b3aaac63b250134110a7f5a6b156982
ttmiirco94 <66681286+ttmiirco94@users.noreply.github.com>
2024-07-05 02:04:09 +0200

    remove older version of TestData solution

--- backend/src/routes/testDataRoutes.js
+++
@@ -1,9 +0,0 @@
-const testDataController = require("../controllers/testDataController");
-const express = require('express');
-const router = express.Router();
-
-router.get('/:testID', testDataController.getTestData);
-router.post('/:testID', (req, res) => { testDataController.storeTestData(req, res, req.app.get('wss')); });
-router.delete('/:testID', (req, res) => { testDataController.deleteTestData(req, res, req.app.get('wss')); });
-
-module.exports = router;
